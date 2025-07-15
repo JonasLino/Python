47a74e32eb51fe60733940ac613911c324005ad0
jonaslino <jonasplino@gmail.com>
2025-07-15 14:10:22 -0300

    Upload

--- RPA/slideshow.py
+++ RPA/slideshow.py
@@ -2,11 +2,20 @@ import time
 import pyautogui
 
 def glpi():
-    time.sleep(10)
+    positions = [(52, 761), (216, 756), (447, 758)]
+    counter = 0
+    
+    while True:
+        for pos in positions:
+            pyautogui.click(x=pos[0], y=pos[1])
+            time.sleep(60)
+            counter += 1
+            
+            if counter >= 10:
+                pyautogui.hotkey("ctrl", "R")
+                counter = 0
 
-    pyautogui.press("F5")
 def main():
-
     glpi()
 
 if __name__ == "__main__":
